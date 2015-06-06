14e77bfa349e9982b1f2a6b75251fa5fc5e638c4
Emunt <wnichols2012@my.fit.edu>
2015-06-06 09:46:09 -0400

    Add a simple fizzbuzz example written in go

---
+++ go/fizzbuzz.go
@@ -0,0 +1,28 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+     var n int
+
+     fmt.Scanf("%d", &n)
+
+     for i := 1; i <= n; i++ {
+     	 if i % 3 == 0 {
+	      fmt.Print("Fizz")
+	 }
+
+	 if i % 5 == 0 {
+	      fmt.Print("Buzz");
+	 }
+
+	 if i % 3 != 0 && i%5 != 0 {
+	    fmt.Print(i)
+	 }
+
+	 fmt.Println()
+     }
+
+}
